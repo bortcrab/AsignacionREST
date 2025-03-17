95feb941ea539dc98e95fde51a2a56cb6c0afa37
bortcrab <bortcrab@gmail.com>
2025-03-17 02:14:10 -0700

    Initial commit

---
+++ ClienteREST/src/main/java/itson/clienterest/ClienteREST.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.clienterest;
+
+/**
+ *
+ * @author Familia
+ */
+public class ClienteREST {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
